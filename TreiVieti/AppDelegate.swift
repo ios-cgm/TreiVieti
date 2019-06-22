9ed06cee7748bafb335ae9ae42b34b146ba0145e
Daniel Munteanu <daniel.munteanu@cgm.com>
2019-06-22 13:01:31 +0300

    added Poppins font

--- TreiVieti/AppDelegate.swift
+++ TreiVieti/AppDelegate.swift
@@ -12,6 +12,21 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+//        for family in UIFont.familyNames.sorted() {
+//            let names = UIFont.fontNames(forFamilyName: family)
+//            print("Family: \(family) Font names: \(names)")
+//        }
+        /*
+         Family: Poppins Font names: ["Poppins-Bold", "Poppins-Italic", "Poppins-BoldItalic", "Poppins-Regular"]
+         Family: Poppins Black Font names: ["Poppins-BlackItalic", "Poppins-Black"]
+         Family: Poppins ExtraBold Font names: ["Poppins-ExtraBold", "Poppins-ExtraBoldItalic"]
+         Family: Poppins ExtraLight Font names: ["Poppins-ExtraLight", "Poppins-ExtraLightItalic"]
+         Family: Poppins Light Font names: ["Poppins-Light", "Poppins-LightItalic"]
+         Family: Poppins Medium Font names: ["Poppins-Medium", "Poppins-MediumItalic"]
+         Family: Poppins SemiBold Font names: ["Poppins-SemiBold", "Poppins-SemiBoldItalic"]
+         Family: Poppins Thin Font names: ["Poppins-ThinItalic", "Poppins-Thin"]
+ */
         return true
     }
 }
