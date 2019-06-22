1e5322e7dcb0cfa6f968793b15c5eee2a1a1d399
Daniel Munteanu <daniel.munteanu@cgm.com>
2019-06-22 16:29:02 +0300

    added funny alert

--- TreiVieti/Screen/Login/LoginViewController.swift
+++ TreiVieti/Screen/Login/LoginViewController.swift
@@ -77,6 +77,9 @@ class LoginViewController: UIViewController {
             emailTextField.text = email
         }
     }
+    @IBAction func forgotPasswordButtonTapped(_ sender: Any) {
+        UIAlertController.showOKAlert(from: self, message: "MemoPlus?")
+    }
 }
 
 extension LoginViewController: UITextFieldDelegate {
